3fe70fc3aeb0f1cbe9edbdf655a7790507de737c
Lorens <gueronlj@gmail.com>
2023-06-18 20:32:55 -0400

    dashboard displays events from a harcoded user

---
+++ components/EventTable/EventTable.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import styles from './styles.module.css';
+
+const EventTable = ({user}) => {
+
+   const [data, setData] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState(null)
+
+   const fetchData = async () => {
+      try{
+         setLoading(true)
+         const response = await axios.post('/api/events/eventsByUser', {
+             userId: 1
+         });
+         setData(response.data)
+         console.log(response.data)
+      }catch(err){
+         setError(err)
+         console.log(err);
+      }finally{
+         setLoading(false)
+      }
+   }
+
+   useEffect(() => {
+      fetchData()
+   },[])
+
+   return (  
+      <>
+         {loading && <p>Loading...</p>}
+         {error && <h2>{error?.code}</h2>}
+         {data &&
+            <div>
+               {data.map((event) => {
+                  return(
+                     <div className={styles.card} key={event.id}>
+                        <h2>{event.title}</h2>
+                        <p>{event.description}</p>
+
+                        <h4>Who's going</h4>
+                        <ul>
+                           {event.User.map((user) => {
+                              return(
+                                 <li key={user.id}>{user.email}</li>
+                              )
+                           })}
+                        </ul>                       
+                     </div>
+                  )
+               })}
+            </div>
+         }       
+      </>     
+   )
+}
+
+export default EventTable;
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -2,17 +2,23 @@ import Layout from "@/components/layout";
 import styles from './dashboard.module.css'
 import SideMenu from "@/components/sidemenu/SideMenu";
 import Router from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import EventTable from "@/components/EventTable/EventTable";
 
 const Dashboard = () => {
+
+   const [currentUser, setCurrentUser] = useState(null)
    useEffect(() => {
    //Attatch Firebase authentication Observer
       const auth = getAuth()
-      const user = auth.currentUser 
+      const user = auth.currentUser
       if(!user) {
+         setCurrentUser(null)
          Router.push('/login')
-      }  
+      }else{
+         setCurrentUser(user);
+      }
    },[])
 
    return (
@@ -21,8 +27,10 @@ const Dashboard = () => {
             <SideMenu/>
             
             <div className = {styles.dashboard}>
-               <h1>Dashboard</h1>
-               <p>Content here</p>
+               <h1>My Events</h1>
+
+               {currentUser && <EventTable
+                  user={currentUser}/>}              
             </div>
          </div>
       </Layout>
